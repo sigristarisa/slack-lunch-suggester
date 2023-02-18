388d1e29b5e4de7312930e43d6a2c33c54a24d87
sigristarisa <arisasigrist.ch@gmail.com>
2023-02-18 14:29:15 +0100

    bot able to respond with simple message

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ require('dotenv').config()
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
 });
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+    await say(`Hey there <@${message.user}>!`);
+  });
+  
 
 (async () => {
   // Start your app

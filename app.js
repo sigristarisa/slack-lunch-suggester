ecc07362dedd19a35ff9c8c8e74cb1947c49e75e
sigristarisa <arisasigrist.ch@gmail.com>
2023-02-19 18:04:39 +0100

    reformatted files and removed console.log

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { App } = require('@slack/bolt');
-const data = require("./locations.json")
-require('dotenv').config()
+const { App } = require("@slack/bolt");
+const data = require("./locations.json");
+require("dotenv").config();
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
@@ -11,23 +11,23 @@ const app = new App({
 });
 
 const getLocation = () => {
-    const locations = data.results
-const location = locations[Math.floor(Math.random() * locations.length)];
-console.log(locations.length)
-    return location
-}
+  const locations = data.results;
+  const location = locations[Math.floor(Math.random() * locations.length)];
+  return location;
+};
 
-app.command('/lunch', async ({ command, ack, respond }) => {
-    await ack()
-    const location = getLocation()
-    const nameToQuery = location.name.replace(/\s/g, "+").replace(/,/g, "%2C")
-    await respond(`Let's go to <https://www.google.com/maps/search/?api=1&query=${nameToQuery}&query_place_id=${location.place_id}|${location.name}> for lunch!`)
-
-  });
+app.command("/lunch", async ({ command, ack, respond }) => {
+  await ack();
+  const location = getLocation();
+  const nameToQuery = location.name.replace(/\s/g, "+").replace(/,/g, "%2C");
+  await respond(
+    `Let's go to <https://www.google.com/maps/search/?api=1&query=${nameToQuery}&query_place_id=${location.place_id}|${location.name}> for lunch!`
+  );
+});
 
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();

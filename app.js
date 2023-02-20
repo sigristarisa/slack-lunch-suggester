5616d4781cfd9732259b958681edabd93865af91
sigristarisa <arisasigrist.ch@gmail.com>
2023-02-20 18:01:49 +0100

    readded events

--- app.js
+++ app.js
@@ -24,6 +24,39 @@ app.command("/lunch", async ({ command, ack, respond }) => {
   );
 });
 
+const challenge = (req, res) => {
+  console.log("req body challenge is:", req.body.challenge);
+
+  res.status(200).send({
+    challenge: req.body.challenge,
+  });
+};
+
+const events = async (req, res) => {
+  const type = req.body.type;
+
+  if (type === "url_verification") {
+    await challenge(req, res);
+  } else if (validateSlackRequest(req, signingSecret)) {
+    if (type === "event_callback") {
+      const event_type = req.body.event.type;
+
+      switch (event_type) {
+        case "app_mention":
+          await app_mention(req, res);
+          break;
+        case "channel_created":
+          await channel_created(req, res);
+          break;
+        default:
+          break;
+      }
+    } else {
+      console.log("body:", req.body);
+    }
+  }
+};
+
 (async () => {
   await app.start(process.env.PORT || 3000);
   console.log("⚡️ Bolt app is running!");
